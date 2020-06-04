363f86b700c0988e6f5f51ed36a51e3ebd9b4d7f
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-06-04 10:47:32 -0700

    added greeting in env doc

--- api/server.js
+++ api/server.js
@@ -8,7 +8,10 @@ const server = express();
 server.use(helmet());
 
 server.get('/', (req, res) =>{
-    res.status(200).json({ environment: process.env.NODE_ENV, port: process.env.PORT });
+    res.status(200).json({ 
+        environment: process.env.NODE_ENV, 
+        port: process.env.PORT, 
+        greeting: process.env.GREETING });
 });
 
 server.use('/api', apiRouter);
